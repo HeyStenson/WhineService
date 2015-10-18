ec6cadaa3b4ef9d3e93b89b5a61a79eab2010519
HeyStenson <h.n.stenson@gmail.com>
2015-10-18 10:16:01 -0700

    first commit

---
+++ wines.js
@@ -0,0 +1,5 @@
+var wines = ["Grenache", "Tepranillo", "Sauvignon blanc", "Syrah", "Pinot noir", "Merlot", "Sangiovese merlot", "Riesling", "Chardonnay", "Zinfandel", "Cabernet sauvignon"]
+
+var describe =["flirtatious", "bold", "childishly playful", "savage", "mysterious", "dense", "aggressive", "demure", "motivated", "futuristic", "fanciful"]
+
+var hintsOf = ["strawberries", "cinnamon", "leather", "sunshine", "despair", "applesauce", "dungeons", "dragons", "black pepper", "truffles", "orange essence", "licorice", "cherry", "murderous rage", "bacon", "acetone"]
