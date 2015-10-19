dcc10ad88469a6e1ba5486beb7b5cf8bc8f7e9ea
HeyStenson <h.n.stenson@gmail.com>
2015-10-18 21:39:44 -0700

    added a little more mock data and some animations

---
+++ README.md
@@ -0,0 +1,7 @@
+WHINE SERVICE:
+
+Got a gripe? Have some grapes! Or, to put it more succinctly: Have some wine with that whine. 
+
+A silly toy app that let me play with Angular.
+
+Dependencies: Angular, Angular Animate, Angular Resource, Animate.css, and Bootstrap.
--- app.js
+++ app.js
@@ -12,10 +12,14 @@ app.controller('WineCtrl', function($scope, $window){
 
 	$scope.hintOf1 = hintsOf[Math.floor(Math.random() * hintsOf.length)];
 	$scope.hintOf2 = hintsOf[Math.floor(Math.random() * hintsOf.length)];
+
+	$scope.platitude = plat[Math.floor(Math.random() * plat.length)];
 });
 
-var hintsOf = ["strawberries", "cinnamon", "leather", "sunshine", "despair", "applesauce", "dungeons", "dragons", "black pepper", "truffles", "orange essence", "licorice", "cherry", "murderous rage", "bacon", "acetone"];
+var hintsOf = ["strawberries", "cinnamon", "leather", "sunshine", "despair", "applesauce", "dungeons", "dragons", "black pepper", "truffles", "orange essence", "licorice", "cherry", "murderous rage", "bacon", "acetone", "cardamom", "lemon", "cantalope", "grass"];
 
-var descriptors =["flirtatious", "bold", "childishly playful", "savage", "mysterious", "dense", "aggressive", "demure", "motivated", "futuristic", "fanciful"];
+var descriptors =["flirtatious", "bold", "childishly playful", "savage", "mysterious", "dense", "aggressive", "demure", "motivated", "futuristic", "fanciful", "childish", "immature", "rustic", "bland", "exotic", "fluffy"];
 
 var wines = ["Grenache", "Tepranillo", "Sauvignon blanc", "Syrah", "Pinot noir", "Merlot", "Sangiovese merlot", "Riesling", "Chardonnay", "Zinfandel", "Cabernet sauvignon"];
+
+var plat = ["Bless your heart.", "Poor baby.", "Aww, that's rough.", "So sorry to hear that.", "DANG.", "What a rough break.", "Poor thing."]
